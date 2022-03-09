7e1786f385370c86db3c89af67dcacf26b7505c0
KatyaDremliuh <nerona.91@gmail.com>
2022-03-09 23:16:41 +0300

    Add project files.

---
+++ Jumpstart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jumpstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
