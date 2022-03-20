dcb7a85d5767f10aed9d1ec4c303828dd662689f
KatyaDremliuh <nerona.91@gmail.com>
2022-03-21 01:17:09 +0300

    Add retrics for plane

--- Jumpstart/Program.cs
+++ Jumpstart/Program.cs
@@ -11,32 +11,33 @@ namespace Jumpstart
             //menu.ShowMenu();
 
             Coordinate destination = new Coordinate(44, 44, 44);
-            Coordinate destination1 = new Coordinate(1144, 1144, 1144);
+            Coordinate destination1 = new Coordinate(1012, 110, 70);
 
 
             IFlyable bird = new Bird();
             IFlyable plane = new Plane();
-            Drone drone = new Drone(10);
-            drone.GetFlyTime(destination);
+            plane.GetFlyTime(destination1);
+            //Drone drone = new Drone(10);
+            //drone.GetFlyTime(destination);
 
-            Drone drone1 = new Drone(10);
-            drone1.GetFlyTime(destination1);
+            //Drone drone1 = new Drone(10);
+            //drone1.GetFlyTime(destination1);
 
-            List<IFlyable> flyingObjects = new List<IFlyable>() { bird, plane, drone };
+            //List<IFlyable> flyingObjects = new List<IFlyable>() { bird, plane, drone };
 
-            bird.FlyTo(destination);
-            foreach (var flyingObject in flyingObjects)
-            {
-                string type = string.Empty;
-                if (flyingObject is Drone)
-                {
-                    type = drone.FlyingObject;
-                }
+            //bird.FlyTo(destination);
+            //foreach (var flyingObject in flyingObjects)
+            //{
+            //    string type = string.Empty;
+            //    if (flyingObject is Drone)
+            //    {
+            //        type = drone.FlyingObject;
+            //    }
 
-                Console.WriteLine(type);
-                flyingObject.GetFlyTime(destination);
-                Console.WriteLine();
-            }
+            //    Console.WriteLine(type);
+            //    flyingObject.GetFlyTime(destination);
+            //    Console.WriteLine();
+            //}
         }
 
         
